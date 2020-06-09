8a5b234d0dfc02b997caec83ea7ff4ef03724358
slonikappa <maxsitron@gmail.com>
2020-06-10 00:13:54 +0300

    DAL Interfaces added

---
+++ SocialBets/SocialBets.Domain.Interfaces/Database/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialBets.Domain.Interfaces.Database
+{
+    interface IEntity<T>
+    {
+        public T Id { get; set; }
+    }
+}
---
+++ SocialBets/SocialBets.Domain.Interfaces/Database/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialBets.Domain.Interfaces.Database
+{
+    interface IRepository<TEntity, TIdType> where 
+        TEntity : class
+    {
+        Task Add(TEntity entity);
+        Task<TEntity> GetItem(TIdType id);
+        Task<List<TEntity>> GetAll();
+        Task Update(TEntity entity);
+        Task DeleteByItem(TEntity entity);
+        Task DeleteById(TIdType id);
+    }
+}
