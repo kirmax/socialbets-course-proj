a547663b556743fe28e05505deb5dc77d7056167
slonikappa <maxsitron@gmail.com>
2020-06-11 14:21:13 +0300

    IRepository<T> constraints edited

--- SocialBets/SocialBets.Domain.Interfaces/Database/IRepository.cs
+++ SocialBets/SocialBets.Domain.Interfaces/Database/IRepository.cs
@@ -5,14 +5,15 @@ using System.Threading.Tasks;
 
 namespace SocialBets.Domain.Interfaces.Database
 {
-    interface IRepository<TEntity, TIdType> where 
-        TEntity : class
+    interface IRepository<TEntity, TId> 
+        where TEntity : class
+        where TId : struct
     {
         Task Add(TEntity entity);
-        Task<TEntity> GetItem(TIdType id);
+        Task<TEntity> GetItem(TId id);
         Task<List<TEntity>> GetAll();
         Task Update(TEntity entity);
         Task DeleteByItem(TEntity entity);
-        Task DeleteById(TIdType id);
+        Task DeleteById(TId id);
     }
 }
