53ef03c61d9ad0074ffde73bce808b6a79cda08a
slonikappa <maxsitron@gmail.com>
2020-06-23 13:50:15 +0300

    OperationsHistory Added

--- SocialBets/SocialBets.Domain.Core/Models/OperationType.cs
+++ SocialBets/SocialBets.Domain.Core/Models/OperationType.cs
@@ -6,15 +6,14 @@ using SocialBets.Domain.Core.Interfaces;
 
 namespace SocialBets.Domain.Core.Models
 {
-    enum OperationType
+    /*enum OperationType
     {
         Income, Withdrawal, Bet
-    }
+    }*/
 
     class Operation : IEntity<int>
     {
         public int Id { get; set; }
-        public OperationType Type { get; set; }
-
+        public string Type { get; set; }
     }
 }
---
+++ SocialBets/SocialBets.Domain.Core/Models/OperationsHistoryItem.cs
@@ -0,0 +1,14 @@
+﻿using SocialBets.Domain.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialBets.Domain.Core.Models
+{
+    class OperationsHistoryItem : IEntity<int>
+    {
+        public int Id { get; set; }
+        public int OperationMoney { get; set; }
+        public Operation OperationId { get; set; }
+    }
+}
