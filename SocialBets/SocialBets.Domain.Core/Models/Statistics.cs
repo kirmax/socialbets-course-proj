a5ca54b04e9fdc8a1dc41f812fb97f6bff7da741
student <student@itstep1.dp.ua>
2020-06-24 13:46:05 +0300

    Models for user added

---
+++ SocialBets/SocialBets.Domain.Core/Models/Statistics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialBets.Domain.Core.Models
+{
+    class Statistics
+    {
+        public int WinsCount { get; set; }
+        public int LosesCount { get; set; }
+    }
+}
