f496714b47da56f08be64a8ec280d230bf4d1b3a
student <student@itstep1.dp.ua>
2020-06-24 14:23:07 +0300

    Birthdate added

--- SocialBets/SocialBets.Domain.Core/Models/UserInfo.cs
+++ SocialBets/SocialBets.Domain.Core/Models/UserInfo.cs
@@ -10,5 +10,6 @@ namespace SocialBets.Domain.Core.Models
         public int Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        public DateTime Birthdate { get; set; }
     }
 }
