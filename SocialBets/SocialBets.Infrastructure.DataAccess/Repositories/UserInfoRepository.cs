9d8ab006c56b6dbbb0e9b20d03ffab97d58b0266
student <student@itstep1.dp.ua>
2020-06-24 14:23:47 +0300

    Repository created

---
+++ SocialBets/SocialBets.Infrastructure.DataAccess/Repositories/UserInfoRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialBets.Infrastructure.DataAccess.Repositories
+{
+    class UserInfoRepository
+    {
+    }
+}
