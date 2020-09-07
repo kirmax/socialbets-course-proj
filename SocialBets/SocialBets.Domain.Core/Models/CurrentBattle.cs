d05461ea33932a96834cd1f9517630d65b9d9d35
slonikappa <student@09-03>
2020-09-07 14:58:12 +0300

    Edited

--- SocialBets/SocialBets.Domain.Core/Models/CurrentBattle.cs
+++ SocialBets/SocialBets.Domain.Core/Models/CurrentBattle.cs
@@ -9,6 +9,8 @@ namespace SocialBets.Domain.Core.Models
     public class CurrentBattle : IEntity<Guid>, IBattle
     {
         public Guid Id { get; set; }
+        public ApplicationUser FirstPlayer { get; set; }
+        public ApplicationUser? SecondPlayer { get; set; }
         public DateTime TimeOfBattle { get; set; }
         public decimal Bet { get; set; }
         public SocialNetwork SocialNetwork { get; set; }
