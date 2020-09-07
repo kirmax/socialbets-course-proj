dea125a3632acba2526aad9c618f12c89ab4e4fb
slonikappa <student@09-03>
2020-09-07 14:58:00 +0300

    Service edited, business logic started

---
+++ SocialBets/SocialBets.Infrastructure.BusinessLogic/BattleService.cs
@@ -0,0 +1,51 @@
+﻿using SocialBets.Domain.Interfaces.Database;
+using SocialBets.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SocialBets.Domain.Core.Models;
+
+namespace SocialBets.Infrastructure.BusinessLogic
+{
+    class BattleService : IBattleService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BattleService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void AttachToBattle()
+        {
+
+            
+            
+            throw new NotImplementedException();
+        }
+
+        public void CancelBattle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task CreateBattle(CurrentBattle battle)
+        {
+            if (battle is null)
+                throw new ArgumentNullException();
+            
+            await _unitOfWork.CurrentBattleRepository.Add(battle);
+        }
+
+        public void StartBattle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopBattle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SocialBets/SocialBets.Services.Interfaces/IBattleService.cs
@@ -0,0 +1,17 @@
+﻿using SocialBets.Domain.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialBets.Services.Interfaces
+{
+    public interface IBattleService
+    {
+        Task CreateBattle(CurrentBattle battle);
+        void AttachToBattle();
+        void CancelBattle();
+        void StartBattle();
+        void StopBattle();
+    }
+}
