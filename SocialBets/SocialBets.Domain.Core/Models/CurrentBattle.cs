db72a920580739a5ff2214db13cdc8501f3867bf
slonikappa <student@09-03>
2020-09-08 14:12:43 +0300

    Revert "Nav prop List doesnt work, commented"
    This reverts commit da2d3e8d94d43a9d1b30958a38bfb3b506120f2a.

--- SocialBets/SocialBets.Domain.Core/Models/CurrentBattle.cs
+++ SocialBets/SocialBets.Domain.Core/Models/CurrentBattle.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 using SocialBets.Domain.Core.Interfaces;
@@ -11,7 +10,7 @@ namespace SocialBets.Domain.Core.Models
     {
         public Guid Id { get; set; }
         public ApplicationUser FirstPlayer { get; set; }
-        public ApplicationUser SecondPlayer { get; set; } = null;
+        public ApplicationUser? SecondPlayer { get; set; }
         public DateTime TimeOfBattle { get; set; }
         public decimal Bet { get; set; }
         public SocialNetwork SocialNetwork { get; set; }
