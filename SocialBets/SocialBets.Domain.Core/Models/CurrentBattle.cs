551e87b72d01390b63aa58d4fe18d2c6fb5263d7
slonikappa <student@09-03>
2020-09-08 14:18:02 +0300

    nullable SecondPlayer

--- SocialBets/SocialBets.Domain.Core/Models/CurrentBattle.cs
+++ SocialBets/SocialBets.Domain.Core/Models/CurrentBattle.cs
@@ -10,7 +10,7 @@ namespace SocialBets.Domain.Core.Models
     {
         public Guid Id { get; set; }
         public ApplicationUser FirstPlayer { get; set; }
-        public ApplicationUser? SecondPlayer { get; set; }
+        public ApplicationUser SecondPlayer { get; set; } = null;
         public DateTime TimeOfBattle { get; set; }
         public decimal Bet { get; set; }
         public SocialNetwork SocialNetwork { get; set; }
